38275a60928bbd93bd0f4eb81782a526efe765ee
Lxzano <arturolozanodelgado@gmail.com>
2026-02-09 14:35:05 +0100

    Estructura de Program

--- EjemploCVArturoLD/Program.cs
+++ EjemploCVArturoLD/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             // CONSTANTES
+
+            // Variables    
+
+            // Inicializacion
+
+            // Entrada
+
+            // Proceso
+
+            // Salida
         }
     }
 }
